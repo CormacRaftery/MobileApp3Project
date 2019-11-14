42910c3f00d34a7dcc8383752d27b099938a7166
cormacraftery <G00348802@gmit.ie>
2019-11-14 04:08:08 +0000

    Loading Level

--- Project/Assets/Scripts/KillEnemyStomp.cs
+++ Project/Assets/Scripts/KillEnemyStomp.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class KillEnemyStomp : MonoBehaviour
 {
     public int damage;
+    public AudioSource stomp;
+    public float bounceOnEnemy;
+    private Rigidbody2D myRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = transform.parent.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,9 +20,15 @@ public class KillEnemyStomp : MonoBehaviour
         
     }
 
-    void OnTriggerEnter2D(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Enemy")
-            Destroy(other);
+        if (other.tag == "Enemies")
+        {
+            stomp.Play();
+            Destroy(other.gameObject);
+            Debug.Log("Stomp");
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, bounceOnEnemy);
+        }
+        
     }
 }
